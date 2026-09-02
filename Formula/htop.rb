8e7060efe27b56446885162746563afffcb6680b
summer <1321067378@qq.com>
2026-09-03 02:54:17 +0800

    htop: add custom bottle for macos Ventura
    - Import formula from homebrew/core (27c3cdd5393fe98396104c896821d9ffeeb81d4b)
    - Add bottle block built for macOS Ventura
    - Direct root_url to custom GitHub Releases

---
+++ Formula/htop.rb
@@ -0,0 +1,53 @@
+class Htop < Formula
+  desc "Improved top (interactive process viewer)"
+  homepage "https://htop.dev/"
+  url "https://github.com/htop-dev/htop/releases/download/3.5.3/htop-3.5.3.tar.xz"
+  sha256 "a8b164386494cb85bb255a415a3f5f80afe7a0c4491da5d113b3a0f951087e65"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/forsummer/localbrew/releases/download"
+    rebuild 1
+    sha256 cellar: :any, ventura: "f8187594f50e5d9264d2395fc97bed9ad209ee9ddb0820fc6e6c7045acb3965a"
+  end
+
+  head do
+    url "https://github.com/htop-dev/htop.git", branch: "main"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "forsummer/localbrew/pkgconf" => :build
+  depends_on "forsummer/localbrew/ncurses" # enables mouse scroll
+
+  on_linux do
+    depends_on "lm-sensors"
+  end
+
+  def install
+    system "./autogen.sh" if build.head?
+    args = ["--prefix=#{prefix}"]
+    args << "--enable-sensors" if OS.linux?
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      htop requires root privileges to correctly display all running processes,
+      so you will need to run `sudo htop`.
+      You should be certain that you trust any software you grant root privileges.
+    EOS
+  end
+
+  test do
+    pipe_output(bin/"htop", "q", 0)
+  end
+end
